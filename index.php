fe8bef171a842dd38f02145f35e706dcbd3de28f
Alby Mukhlis <albytita10@gmail.com>
2022-10-15 18:52:37 +0700

    TugasPraUTS1

---
+++ index.php
@@ -0,0 +1,236 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>QUEENBAND</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <style>
+        .nav-item {
+            padding-right: 50px;
+        }
+
+        .carousel-item img {
+            height: 100vh;
+        }
+
+        p {
+            text-align: justify;
+            color:#fff;
+        }
+        body {
+            background-color:black;
+        }
+        h1,h3{
+            color:#fff;
+        }
+        h6{
+            color:grey;
+        }
+    </style>
+  </head>
+  <body>
+  
+<!-- NAVBAR -->
+<nav id="navbar-custom" class="navbar navbar-expand-lg fixed-top navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><h3><u>QUEEN BAND OFFICIAL</u></h3></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#location">Location</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#news">News</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+        
+    </div>
+  </div>
+</nav>
+
+<!-- HOME -->
+<section id="home">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="images/queenlogo.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="images/queenband.webp" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="images/bohemian.png" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+</section>
+
+<!-- ABOUT -->
+<section id="about" style="padding-top:50px">
+    <div class="container">
+        <h1 class="text-center">QUEEN ARE :</h1>
+        <div class="row" style="padding-top:50px" data-aos="zoom-in">
+            <div class="col-md-6">
+                <h3>Fredddie Mercury</h3>
+                <h6>-Lead Vocal, Guitar, Piano-</h6>
+                <p>Freddie Mercury (born Farrokh Bulsara; 5 September 1946 – 24 November 1991)[2] was a British singer and songwriter, who achieved worldwide fame as the lead vocalist of the rock band Queen. Regarded as one of the greatest singers in the history of rock music, he was known for his flamboyant stage persona and four-octave vocal range. Mercury defied the conventions of a rock frontman with his theatrical style, influencing the artistic direction of Queen. Mercury was diagnosed with AIDS in 1987. He continued to record with Queen, and posthumously featured on their final album, Made in Heaven (1995). He announced his diagnosis the day before his death, from complications from the disease, in 1991 at the age of 45. In 1992, a concert in tribute to him was held at Wembley Stadium, in benefit of AIDS awareness. His career with Queen was dramatised in the 2018 biopic Bohemian Rhapsody.
+
+As a member of Queen, Mercury was posthumously inducted into the Rock and Roll Hall of Fame in 2001, the Songwriters Hall of Fame in 2003, and the UK Music Hall of Fame in 2004. In 1990, he and the other Queen members were awarded the Brit Award for Outstanding Contribution to British Music, and one year after his death, Mercury was awarded it individually. In 2005, Queen were awarded an Ivor Novello Award for Outstanding Song Collection from the British Academy of Songwriters, Composers, and Authors. In 2002, Mercury was voted number 58 in the BBC's poll of the 100 Greatest Britons.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="images/freddiemercury.jpg" class="img-rounded" width="100%"/>
+            </div>
+        </div>
+        <div class="row" style="padding-top:100px" data-aos="zoom-in">
+            <div class="col-md-6">
+                <img src="images/brianmay.jpg" class="img-rounded" width="100%"/>
+            </div>
+            <div class="col-md-6">
+                <h3>Brian May</h3>
+                <h6>-Lead Guitar, Backing Vocal, Piano-<h6>
+                <p>Brian Harold May CBE (born 19 July 1947) is an English guitarist, singer, composer, and astrophysicist. He achieved fame as lead guitarist of the rock band Queen. May was a co-founder of Queen with lead singer Freddie Mercury and drummer Roger Taylor. His songwriting contributions helped Queen become one of the most successful bands in rock history.Following the death of Mercury in 1991, aside from the 1992 tribute concert, the release of Made in Heaven (1995) and the 1997 tribute single to Mercury, "No-One but You (Only the Good Die Young)" (written by May), Queen were put on hiatus for several years but were eventually reconvened by May and Taylor for further performances featuring other vocalists. In 2005, a Planet Rock poll saw May voted the seventh-greatest guitarist of all time. He was ranked at No. 26 on Rolling Stone's list of the "100 Greatest Guitarists of All Time". In 2012, he was ranked the second-greatest guitarist in a Guitar World magazine readers poll. In 2001, May was inducted into the Rock and Roll Hall of Fame as a member of Queen and in 2018 the band received the Grammy Lifetime Achievement Award.</p>
+            </div>
+        </div>
+        <div class="row" style="padding-top:100px" data-aos="zoom-in">
+            <div class="col-md-6">
+                <h3>Roger Taylor</h3>
+                <h6>-Drummer, Backing Vocal, Percussion-</h6>
+                <p>Roger Meddows Taylor OBE (born 26 July 1949) is an English musician, singer and multi-instrumentalist, best known as the drummer for the rock band Queen. As a drummer, Taylor was recognised early in his career for his unique sound. He was voted the eighth-greatest drummer in classic rock music history in a listener poll conducted by Planet Rock in 2005. He was inducted into the Rock and Roll Hall of Fame in 2001 as a member of Queen. In addition to his drum work, Taylor is well known for his falsetto vocal range. He sometimes played keyboards, guitars and bass on his own songs. During the 1980s, in addition to his work with Queen, he formed a parallel band known as the Cross, in which he was the lead singer and rhythm guitarist. During the early 1980s, Taylor was also a panellist on the popular UK quiz show Pop Quiz, hosted by Mike Read. In 2014, he appeared in The Life of Rock with Brian Pern as himself.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="images/rogertaylor2.jpg" class="img-rounded" width="100%"/>
+            </div>
+        </div>
+        <div class="row" style="padding-top:100px" data-aos="zoom-in">
+            <div class="col-md-6">
+                <img src="images/johndeacon.jpg" class="img-rounded" width="100%"/>
+            </div>
+            <div class="col-md-6">
+                <h3>John Deacon</h3>
+                <h6>-Bass, Backing Vocal-<h6>
+                <p>John Richard Deacon (born 19 August 1951) is an English retired musician, best known for being the bassist for the rock band Queen. He composed several songs for the group, including Top 10 hits "You're My Best Friend", "Another One Bites the Dust", and "I Want to Break Free"; co-wrote "Under Pressure", "Friends Will Be Friends" and "One Vision"; and he was involved in the band's financial management.
+
+Deacon grew up in Oadby, Leicestershire, playing bass in a local band, The Opposition, before moving to study electronics at Chelsea College, London. He joined Queen in 1971 on the strength of his musical and electronic skills, particularly the home-made Deacy Amp which guitarist Brian May used to create guitar orchestras throughout Queen's career. From the third album, Sheer Heart Attack, onwards, he wrote at least one song per album, several of which became hits. As well as bass, Deacon played some guitar, keyboards and piano on Queen's studio work.
+
+After the death of lead singer Freddie Mercury in 1991 and the following year's Tribute Concert, Deacon performed only sporadically with the remaining members of Queen before retiring from the music industry in 1997 after recording "No-One but You (Only the Good Die Young)". He has not performed on any of the other projects that the two remaining members, Brian May and Roger Taylor, have put together. He was inducted into the Rock and Roll Hall of Fame as a member of Queen in 2001 and into Songwriters Hall of Fame in 2003.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- LOCATION -->
+<section id="location" style="padding-top:50px;">
+    <div class="container-fluid">
+        <h1 class="text-center">Office Location</h1>
+        <div class="row">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2484.088341923209!2d-0.22360908457754652!3d51.49324631955956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48760fb8723aaaad%3A0x696d62a398abe2fe!2sUniversal%20Music%20Office!5e0!3m2!1sen!2sid!4v1665828473334!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+</section>
+
+<!-- NEWS -->
+<section id="news" style="padding-top:50px;">
+<div class="container">
+<h1 class="text-center">NEWS</h1>
+<hr>
+<div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary">New Single</strong>
+          <h3 class="mb-0">"Face it Alone" Released !!!</h3>
+          <div class="mb-1 text-muted">Oct 15, 2022</div>
+          <p class="card-text mb-auto">Queen have released a new song in 13th October. Well, it’s not exactly new. It’s an unreleased song they recorded in 1988 when legendary lead singer Freddie Mercury was still alive. ‘Face it Alone’ was recorded in the sessions for Queen’s 1989 album ‘The Miracle’. The band had forgotten about the recording but rediscovered it when they returned to the sessions as part of a reissue box-set for the album, set to be released 18 November.                 </p>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <img style="padding-top:158px; padding-right:20px" width="200" src="images/thumbface.webp">
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary">Queen Tour 2022</strong>
+          <h3 class="mb-0">Queen+Adam Lambert Gear Up For Much-Anticipated UK & Europe ‘Rhapsody’ Tour</h3>
+          <div class="mb-1 text-muted">May 16, 2022</div>
+          <p class="card-text mb-auto">Twice postponed due to the Covid-19 pandemic, the much-anticipated ‘Rhapsody’ Tour by Queen+Adam Lambert is finally all set to rock its way across the UK and Europe for eight weeks from late May to late July. The show has been hailed as “the latest astonishing chapter in the story of rock’s greatest band, putting a brand new, highly distinctive spin on over five decades of Queen’s music.</p>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <img style="padding-top:158px; padding-right:20px" width="200" src="images/queenadamlambert.jpg">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  </section>
+<!-- CONTACT -->
+<section id="contact" style="height:300px;" class='bg-danger'>
+    <div class="container">
+        <div class="row" style="padding-top:50px">
+            <div class="col-md-6 text-white">
+                <img src="images/queenfooter.png" width="100px"/>
+                <br>
+                <br>
+                <h5>Contact :</h5> 
+                <p>Publicity : Phil Symes - philsymes@theprcontact.com</p>
+                <p>Queenonline.com : Nick Weymouth - nick@moonlightmile.co.uk</p>
+                <p>Queen Fan Club : Jacky Smith - qfc@queenworld.com</p>
+            </div>
+            <div class="col-md-6">
+            </div>
+        </div>
+    </div>
+</section>
+<footer class="container-fluid" style="height:50px">
+    <div class="row">
+            <span class="text-center" style="padding:15px">Copyright@2022</span>
+    </div>
+</footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+        let navbar = document.getElementById('navbar-custom');
+
+        window.addEventListener('scroll', function(){
+            let scroll = window.scrollY;
+
+            if (scroll > 50) {
+                navbar.classList.add('bg-danger');
+                // navbar.classList.remove('navbar-dark');
+            } else {
+                navbar.classList.remove('bg-danger');
+                // navbar.classList.add('navbar-dark');
+            }
+        });
+    </script>
+  </body>
+</html>
